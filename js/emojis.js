9dee8b27d7213fc3b3f967969fe1152e70626ece
Street Money <mutuadavid93@gmail.com>
2016-11-27 16:04:41 +0300

    Deleted some trash

--- js/emojis.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-   var source = "https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole";
-   
-   $.getJSON(source, function (data) {
-      console.log(data); 
-      //$('#emojis img').attr("src", data.hear_no_evil);
-   });
-});
